08e3e11eb84a28eb081e578fdb8a4dc27acf4ad2
coderstory <coderstory@163.com>
2018-01-29 16:23:43 +0800

    格式化代码
    (cherry picked from commit 95d0e3c)

--- app/src/main/java/com/coderstory/Purify/plugins/start.java
+++ app/src/main/java/com/coderstory/Purify/plugins/start.java
@@ -25,6 +25,7 @@ public class start implements IXposedHookZygoteInit, IXposedHookLoadPackage {
         new MiuiHome().handleLoadPackage(lpparam);
         new CorePatch().handleLoadPackage(lpparam);
         new ThemePatcher().handleLoadPackage(lpparam);
+        new MiuiRoot().handleLoadPackage(lpparam);
     }
 
     @Override
